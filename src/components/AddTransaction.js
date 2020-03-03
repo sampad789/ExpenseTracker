f6207db2231fa63157b27656eec0b7282c19aecf
sampad789 <sampad789@gmail.com>
2020-03-03 19:45:04 +0200

    Working with transactions

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -9,7 +9,7 @@ export const AddTransaction = () => {
       <h3>Add new transaction</h3>
       <form>
         <div className="form-control">
-          <label htmlFor="text">Expense on:</label>
+          <label htmlFor="text">Text:</label>
           <input
             type="text"
             value={text}
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,5 +1,5 @@
 import React, { createContext, useReducer } from "react";
-
+import AppReducer from "./AppReducer";
 //Initial State
 
 const initialState = {
@@ -9,4 +9,24 @@ const initialState = {
     { id: 3, text: "Book", amount: -10 },
     { id: 4, text: "Camera", amount: 150 }
   ]
+};
+
+// Create context
+
+export const GlobalContext = createContext(initialState);
+
+//Provider component
+
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
 };
