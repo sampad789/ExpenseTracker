7955c81f911790354ae0ebbb18d3592c6f8b19b1
sampad789 <sampad789@gmail.com>
2020-03-04 12:09:29 +0200

    Routes Configured

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require("express");
 const dotenv = require("dotenv");
 const colors = require("colors");
 const morgan = require("morgan");
+const connectDB = require("./config/db");
 
-// importing the env variables
+//temporary import
+//const bodyParser = require("body-parser");
+
+// importing the environment variables
 dotenv.config({ path: "./config/config.env" });
 
 //importing routes
@@ -12,6 +16,20 @@ const transactions = require("./routes/transactions");
 // Initializing express
 const app = express();
 
+//TODO  remove this is no errors without body parser
+// // parse application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: false }));
+
+// // parse application/json
+// app.use(bodyParser.json());
+
+// NOTE This can be used instead  of body-parser middleware
+app.use(express.json());
+
+//Initializing mongodb
+
+connectDB();
+
 app.use("/api/v1/transactions", transactions);
 
 const PORT = process.env.PORT || 5000;
@@ -19,6 +37,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
   console.log(
-    `Server running in ${process.env.NODE_ENV}mode on port ${PORT}`.yellow.bold
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
   )
 );
